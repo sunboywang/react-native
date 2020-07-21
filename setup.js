0b54f779a9cf1ac040b6a90854bc8b6598cea278
sunboywang <153226057@qq.com>
2020-07-21 16:04:54 +0800

    Update setup.js
    删除复制的行

--- setup.js
+++ setup.js
@@ -6,9 +6,6 @@ export default class setup extends React.Component {
     return (
       <SafeAreaView>
         <Text style={styles.test}>Hello react-native</Text>
-        <Text style={styles.test}>Hello react-native</Text>
-        <Text style={styles.test}>Hello react-native</Text>
-        <Text style={styles.test}>Hello react-native</Text>
         <LifecycleComponent />
       </SafeAreaView>
     );
